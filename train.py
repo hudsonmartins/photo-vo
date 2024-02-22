bac73d178afa6dc7c79d796e405732c2d355c75b
hudsonmartins <hudsonbr95@gmail.com>
2024-02-22 17:31:51 -0500

    feat: loading data and model/performing inference/plotting debug images

--- train.py
+++ train.py
@@ -1,19 +1,155 @@
+import torch
 import argparse
+import matplotlib.pyplot as plt
 from omegaconf import OmegaConf
+from gluefactory import logger
 from gluefactory.datasets import get_dataset
+from gluefactory.models import get_model
+from gluefactory.visualization.viz2d import plot_heatmaps, plot_image_grid
+from gluefactory.utils.experiments import get_best_checkpoint, get_last_checkpoint, save_experiment
+from gluefactory.utils.tensor import batch_to_device
+from utils import draw_kpts, get_patches
 
 
-def main(conf):
+default_train_conf = {
+    "seed": "???",  # training seed
+    "epochs": 1,  # number of epochs
+    "optimizer": "adam",  # name of optimizer in [adam, sgd, rmsprop]
+    "opt_regexp": None,  # regular expression to filter parameters to optimize
+    "optimizer_options": {},  # optional arguments passed to the optimizer
+    "lr": 0.001,  # learning rate
+    "lr_schedule": {
+        "type": None,  # string in {factor, exp, member of torch.optim.lr_scheduler}
+        "start": 0,
+        "exp_div_10": 0,
+        "on_epoch": False,
+        "factor": 1.0,
+        "options": {},  # add lr_scheduler arguments here
+    },
+    "lr_scaling": [(100, ["dampingnet.const"])],
+    "eval_every_iter": 1000,  # interval for evaluation on the validation set
+    "save_every_iter": 5000,  # interval for saving the current checkpoint
+    "log_every_iter": 200,  # interval for logging the loss to the console
+    "log_grad_every_iter": None,  # interval for logging gradient hists
+    "test_every_epoch": 1,  # interval for evaluation on the test benchmarks
+    "keep_last_checkpoints": 10,  # keep only the last X checkpoints
+    "load_experiment": None,  # initialize the model from a previous experiment
+    "median_metrics": [],  # add the median of some metrics
+    "recall_metrics": {},  # add the recall of some metrics
+    "pr_metrics": {},  # add pr curves, set labels/predictions/mask keys
+    "best_key": "loss/total",  # key to use to select the best checkpoint
+    "dataset_callback_fn": None,  # data func called at the start of each epoch
+    "dataset_callback_on_val": False,  # call data func on val data?
+    "clip_grad": None,
+    "pr_curves": {},
+    "plot": None,
+    "submodules": [],
+}
+default_train_conf = OmegaConf.create(default_train_conf)
+
+def main(args):
+    logger.info('Training with the following configuration: ')
+    conf = OmegaConf.load(args.conf)
+    logger.info(conf.data)
+    device ="cpu"# "cuda" if torch.cuda.is_available() else "cpu"
+    logger.info(f"Using device {device}")
+    
+    if args.restore:
+        logger.info(f"Restoring from previous training of {args.experiment}")
+        try:
+            init_cp = get_last_checkpoint(args.experiment, allow_interrupted=False)
+        except AssertionError:
+            init_cp = get_best_checkpoint(args.experiment)
+        logger.info(f"Restoring from checkpoint {init_cp.name}")
+        init_cp = torch.load(str(init_cp), map_location="cpu")
+        conf = OmegaConf.merge(OmegaConf.create(init_cp["conf"]), conf)
+        conf.train = OmegaConf.merge(default_train_conf, conf.train)
+        epoch = init_cp["epoch"] + 1
+
+        # get the best loss or eval metric from the previous best checkpoint
+        best_cp = get_best_checkpoint(args.experiment)
+        best_cp = torch.load(str(best_cp), map_location="cpu")
+        best_eval = best_cp["eval"][conf.train.best_key]
+        del best_cp
+    else:
+        # we start a new, fresh training
+        conf.train = OmegaConf.merge(default_train_conf, conf.train)
+        epoch = 0
+        best_eval = float("inf")
+        if conf.train.load_experiment:
+            logger.info(f"Will fine-tune from weights of {conf.train.load_experiment}")
+            # the user has to make sure that the weights are compatible
+            try:
+                init_cp = get_last_checkpoint(conf.train.load_experiment)
+            except AssertionError:
+                init_cp = get_best_checkpoint(conf.train.load_experiment)
+            # init_cp = get_last_checkpoint(conf.train.load_experiment)
+            init_cp = torch.load(str(init_cp), map_location="cpu")
+            # load the model config of the old setup, and overwrite with current config
+            conf.model = OmegaConf.merge(
+                OmegaConf.create(init_cp["conf"]).model, conf.model
+            )
+        else:
+            init_cp = None
+
     dataset = get_dataset(conf.data.name)(conf.data)
     train_loader = dataset.get_data_loader("train")
     print(f"Training loader has {len(train_loader)} batches")
+
+    if(args.debug):
+        logger.warn('Debugging mode enabled')
+        images, depths, images_kpts = [], [], []
+        
+
+
+    model = get_model(conf.model.name)(conf.model).to(device)
+    if args.compile:
+        model = torch.compile(model, mode=args.compile)
+    loss_fn = model.loss
+    if init_cp is not None:
+        model.load_state_dict(init_cp["model"], strict=False)
+
+    #FOR NOW WE ARE ONLY TESTING THE MODEL
+    model.eval()
+
     for it, data in enumerate(train_loader):
-        print(data)
+        data = batch_to_device(data, device, non_blocking=True)
+        pred = model(data)
+        #pred contains 'keypoints0' 'keypoints1' 'descriptors0' 'descriptors1'
+
+        if(args.debug and it < 4):
+            images.append(
+                [
+                    data[f"view{i}"]["image"][0].permute(1, 2, 0)
+                    for i in range(dataset.conf.views)
+                ]
+            )
+
+            depths.append(
+                [data[f"view{i}"]["depth"][0] for i in range(dataset.conf.views)]
+            )
+            
+            
+    if(args.debug):
+        axes = plot_image_grid(images, dpi=100)
+        for i in range(len(images)):
+            plot_heatmaps(depths[i], axes=axes[i])
+        plt.show()     
+
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("experiment", type=str)
     parser.add_argument("--conf", type=str)
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--restore", action="store_true")
+    parser.add_argument(
+        "--compile",
+        default=None,
+        type=str,
+        choices=["default", "reduce-overhead", "max-autotune"],
+    )
+
     args = parser.parse_args()
-    conf = OmegaConf.load(args.conf)
-    print(conf.data)
-    main(conf)
+    main(args)
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import torch
 
+def draw_kpts(img, kpts, color=(0, 0, 255), radius=5):
+    """Draw keypoints on an image"""
+    for pt in kpts:
+        img = cv2.circle(img, (pt[0], pt[1]), radius, color, 2)
+    return img
 
 def get_patches(img, kpts, patch_size=10):
     """Given an image and a set of keypoints, return the patches around the keypoints"""
@@ -24,3 +29,22 @@ def get_patches(img, kpts, patch_size=10):
 
     return torch.stack(patches)
 
+
+if __name__ == "__main__":
+    import cv2
+    import matplotlib.pyplot as plt
+
+    img = cv2.imread("/home/hudson/Desktop/Unicamp/datasets/megadepth1500/images/0015/29307281_d7872975e2_o.jpg")
+    img = torch.from_numpy(img).permute(2, 0, 1).float() / 255
+    img = img.unsqueeze(0)
+    kpts = torch.tensor([[100, 10], [20, 200], [1200, 900], [500, 500]]).unsqueeze(0).float()
+    print('img size ', img.size())
+    print('kpts shape ', kpts.size())   
+    patches = get_patches(img, kpts)
+    
+    img = img.squeeze(0).permute(1, 2, 0).numpy()
+    kpts = kpts.squeeze(0).numpy().astype(int)
+    for pt in kpts:
+        img = cv2.circle(img, (int(pt[0]), int(pt[1])), 5, (0, 0, 255), 2)
+    
+    print('patches ', patches.size())
