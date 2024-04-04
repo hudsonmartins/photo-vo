44a45690e36e7ed3dafc93bbfa11d3af10b25a5e
hudsonmartins <hudsonbr95@gmail.com>
2024-04-04 17:56:10 -0400

    feat: organizing loss

--- loss.py
+++ loss.py
@@ -1,21 +1,22 @@
 import torch
 from utils import matrix_to_euler_angles
 
-def get_ssim(Ia, Ib):
+def get_ssim(img0, img1):
     #Extracted from https://github.com/mrharicot/monodepth/blob/master/monodepth_model.py#L91
     C1 = 0.01**2
     C2 = 0.03**2
-    mean_a = torch.nn.functional.avg_pool2d(Ia, 3, 1)
-    mean_b = torch.nn.functional.avg_pool2d(Ib, 3, 1)
-    std_a  = torch.nn.functional.avg_pool2d(Ia ** 2, 3, 1) - mean_a ** 2
-    std_b  = torch.nn.functional.avg_pool2d(Ib ** 2, 3, 1) - mean_b ** 2
-    std_ab = torch.nn.functional.avg_pool2d(Ia * Ib , 3, 1) - mean_a * mean_b
+    mean0 = torch.nn.functional.avg_pool2d(img0, 3, 1)
+    mean1 = torch.nn.functional.avg_pool2d(img1, 3, 1)
+    std0  = torch.nn.functional.avg_pool2d(img0 ** 2, 3, 1) - mean0 ** 2
+    std1  = torch.nn.functional.avg_pool2d(img1 ** 2, 3, 1) - mean1 ** 2
+    std01 = torch.nn.functional.avg_pool2d(img0 * img1 , 3, 1) - mean0 * mean1
     
-    SSIM_n = (2 * mean_a * mean_b + C1) * (2 * std_ab + C2)
-    SSIM_d = (mean_a ** 2 + mean_b ** 2 + C1) * (std_a + std_b + C2)
+    SSIM_n = (2 * mean0 * mean1 + C1) * (2 * std01 + C2)
+    SSIM_d = (mean0 ** 2 + mean1 ** 2 + C1) * (std0 + std1 + C2)
 
     SSIM = SSIM_n / SSIM_d
-    return SSIM
+    # Return the average SSIM across the batch
+    return torch.mean(SSIM)
 
 
 def photometric_loss(img0, img1):
@@ -26,6 +27,7 @@ def photometric_loss(img0, img1):
     alpha = 0.85 #Zhao et al
     pe = alpha * ssim_loss + (1-alpha) * l1_loss
     return pe
+
 
 def patches_photometric_loss(patches0, patches1):
     batch_size = patches0.size(0)
