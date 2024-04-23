97c0a0952ddb83981e3a2db5893364a100e3043e
hudsonmartins <hudsonbr95@gmail.com>
2024-04-23 18:03:50 -0400

    feat: loading state dict

--- train.py
+++ train.py
@@ -1,49 +1,37 @@
+import os
 import torch
-import tqdm
+import glob
 import argparse
+import numpy as np
+import random
 import matplotlib.pyplot as plt
 from omegaconf import OmegaConf
 from gluefactory import logger
 from gluefactory.datasets import get_dataset
 from gluefactory.utils.tensor import batch_to_device
-from gluefactory.utils.experiments import get_last_checkpoint, get_best_checkpoint
 from torch.utils.tensorboard import SummaryWriter
 
 from utils import debug_batch, get_sorted_matches
 from model import get_photo_vo_model
 
 default_train_conf = {
-    "seed": "???",  # training seed
+    "seed": 42,  # random seed
     "epochs": 1,  # number of epochs
     "optimizer": "adam",  # name of optimizer in [adam, sgd, rmsprop]
-    "opt_regexp": None,  # regular expression to filter parameters to optimize
     "optimizer_options": {},  # optional arguments passed to the optimizer
     "lr": 0.001,  # learning rate
     "eval_every_iter": 1000,  # interval for evaluation on the validation set
     "save_every_iter": 5000,  # interval for saving the current checkpoint
     "log_every_iter": 200,  # interval for logging the loss to the console
-    "log_grad_every_iter": None,  # interval for logging gradient hists
-    "test_every_epoch": 1,  # interval for evaluation on the test benchmarks
-    "keep_last_checkpoints": 10,  # keep only the last X checkpoints
     "load_experiment": None,  # initialize the model from a previous experiment
-    "median_metrics": [],  # add the median of some metrics
-    "recall_metrics": {},  # add the recall of some metrics
-    "pr_metrics": {},  # add pr curves, set labels/predictions/mask keys
-    "best_key": "loss/total",  # key to use to select the best checkpoint
-    "dataset_callback_fn": None,  # data func called at the start of each epoch
-    "dataset_callback_on_val": False,  # call data func on val data?
-    "clip_grad": None,
-    "pr_curves": {},
-    "plot": None,
-    "submodules": [],
     "tensorboard_dir": "runs",  # directory for tensorboard logs
+    "best_loss": float("inf"),  # best loss for the model
 }
 default_train_conf = OmegaConf.create(default_train_conf)
 
-
 def train(model, train_loader, val_loader, optimizer, device, config, debug=False):
     writer = SummaryWriter(log_dir=config.train.tensorboard_dir)
-    best_loss = float("inf")
+
     for it, data in enumerate(train_loader):
         model.train()
         optimizer.zero_grad()
@@ -74,7 +62,7 @@ def train(model, train_loader, val_loader, optimizer, device, config, debug=Fals
         if(it % config.train.eval_every_iter == 0 or it == (len(train_loader) - 1)):
             model.eval()
             avg_losses = {k: 0 for k in loss.keys()}
-            #for it, data in enumerate(tqdm(val_loader, desc="Validation", ascii=True)):
+
             for it, data in enumerate(val_loader):
                 data = batch_to_device(data, device, non_blocking=True)
                 with torch.no_grad():
@@ -96,15 +84,16 @@ def train(model, train_loader, val_loader, optimizer, device, config, debug=Fals
             fig = debug_batch(output, n_pairs=3)
             writer.add_figure("val/fig/debug", fig, it)        
         
-            if(avg_losses['total'] < best_loss):
+            if(avg_losses['total'] < config.train.best_loss):
                     best_loss = avg_losses['total']
+                    config.train.best_loss = best_loss
                     logger.info(f"Found best model with loss {best_loss}. Saving checkpoint.")
                     torch.save({
                         "model": model.state_dict(),
                         "optimizer": optimizer.state_dict(),
                         "conf": OmegaConf.to_container(config, resolve=True),
                         "epoch": it,
-                    }, f"checkpoint_best.tar") 
+                    }, os.path.join(args.experiment, "best_model.tar"))
 
         if(it % config.train.save_every_iter == 0 or it == (len(train_loader) - 1)):
             logger.info(f"Saving checkpoint at iteration {it}")
@@ -113,7 +102,7 @@ def train(model, train_loader, val_loader, optimizer, device, config, debug=Fals
                 "optimizer": optimizer.state_dict(),
                 "conf": OmegaConf.to_container(config, resolve=True),
                 "epoch": it,
-            }, f"checkpoint_{it}.tar")
+            }, os.path.join(args.experiment, f"checkpoint_{it}.tar"))
 
 
 def main(args):
@@ -126,34 +115,39 @@ def main(args):
     
     if args.restore:
         logger.info(f"Restoring from previous training of {args.experiment}")
-        try:
-            init_cp = get_last_checkpoint(args.experiment, allow_interrupted=False)
-        except AssertionError:
-            init_cp = get_best_checkpoint(args.experiment)
-        logger.info(f"Restoring from checkpoint {init_cp.name}")
-        init_cp = torch.load(str(init_cp), map_location="cpu")
+        ckpts = glob.glob(os.path.join(args.experiment, "checkpoint_*.tar"))
+        ckpts = [os.path.basename(ckpt) for ckpt in ckpts]
+        print('ckpts', ckpts)
+        if len(ckpts) > 0:
+            init_cp_name = sorted(ckpts)[-1]
+            print('init_cp_name', init_cp_name)
+            init_cp = torch.load(os.path.join(args.experiment, init_cp_name), map_location="cpu")
+        else:
+            init_cp = torch.load(os.path.join(args.experiment, "best_model.tar"), map_location="cpu")
+        
         conf = OmegaConf.merge(OmegaConf.create(init_cp["conf"]), conf)
         conf.train = OmegaConf.merge(default_train_conf, conf.train)
-
-        # get the best loss or eval metric from the previous best checkpoint
-        best_cp = get_best_checkpoint(args.experiment)
-        best_cp = torch.load(str(best_cp), map_location="cpu")
-        del best_cp
     else:
         # we start a new, fresh training
         conf.train = OmegaConf.merge(default_train_conf, conf.train)
         if conf.train.load_experiment:
             logger.info(f"Will fine-tune from weights of {conf.train.load_experiment}")
-            # the user has to make sure that the weights are compatible
-            try:
-                init_cp = get_last_checkpoint(conf.train.load_experiment)
-            except AssertionError:
-                init_cp = get_best_checkpoint(conf.train.load_experiment)
-            # init_cp = get_last_checkpoint(conf.train.load_experiment)
-            init_cp = torch.load(str(init_cp), map_location="cpu")
+            ckpts = glob.glob(os.path.join(args.experiment, "checkpoint_*.tar"))
+            ckpts = [os.path.basename(ckpt) for ckpt in ckpts]
+            print('ckpts', ckpts)
+            if len(ckpts) > 0:
+                init_cp_name = sorted(ckpts)[-1]
+                print('init_cp_name', init_cp_name)
+                init_cp = torch.load(os.path.join(args.experiment, init_cp_name), map_location="cpu")
+            else:
+                init_cp = torch.load(os.path.join(args.experiment, "best_model.tar"), map_location="cpu")
+
             # load the model config of the old setup, and overwrite with current config
-            conf.model = OmegaConf.merge(
-                OmegaConf.create(init_cp["conf"]).model, conf.model
+            conf.photo_vo = OmegaConf.merge(
+                OmegaConf.create(init_cp["conf"]).photo_vo, conf.photo_vo
+            )
+            conf.features_model = OmegaConf.merge(
+                OmegaConf.create(init_cp["conf"]).features_model, conf.features_model
             )
         else:
             init_cp = None
@@ -165,18 +159,25 @@ def main(args):
         "rmsprop": torch.optim.RMSprop,
     }[conf.train.optimizer]
 
+    random.seed(conf.train.seed)
+    torch.manual_seed(conf.train.seed)
+    np.random.seed(conf.train.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(conf.train.seed)
+        torch.cuda.manual_seed_all(conf.train.seed)
+
     dataset = get_dataset(conf.data.name)(conf.data)
     train_loader = dataset.get_data_loader("train")
     val_loader = dataset.get_data_loader("val")
-    print(f"Training loader has {len(train_loader)} batches")
-    print(f"Validation loader has {len(val_loader)} batches")
+    logger.info(f"Training with {len(train_loader)} batches and validating with {len(val_loader)} batches")
 
+    os.makedirs(args.experiment, exist_ok=True)
     photo_vo_model = get_photo_vo_model(conf)
-    photo_vo_model.matcher.to(device)
-    if args.compile:
-        photo_vo_model.matcher = torch.compile(photo_vo_model.matcher, mode=args.compile)
+    photo_vo_model.to(device)
+
     if init_cp is not None:
         photo_vo_model.load_state_dict(init_cp["model"], strict=False)
+        logger.info(f"Loaded model from {args.experiment}")
     
     optimizer = optimizer_fn(
         photo_vo_model.parameters(), lr=conf.train.lr, **conf.train.optimizer_options
